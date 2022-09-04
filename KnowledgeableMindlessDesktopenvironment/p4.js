466de10db66c59af038931cf6b8a0dfb87888b45
Kunjan Ballawar <kballawar0@gmail.com>
2022-09-04 19:04:44 +0530

    variables problems

---
+++ KnowledgeableMindlessDesktopenvironment/p4.js
@@ -0,0 +1,11 @@
+
+var name = "Kunjan";
+var school = "kdk school";
+var grade = "A";
+var section = "B";
+var roll_no = 28;
+var mark1 = "math = 97"
+var mark2 = "science = 98"
+var mark3 = "history = 90"
+  
+console.log(name,school,grade,section,roll_no,mark1,mark2,mark3);
